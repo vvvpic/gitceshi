6d3e3bb902b6cf07854cbdb4aa4a31a3f5f1a01a
vvvpic <860866076@qq.com>
2016-05-08 16:36:53 +0800

    初始化提交项目

---
+++ app/src/main/java/ceshi/androidstudio/com/gitceshi/MainActivity.java
@@ -0,0 +1,13 @@
+package ceshi.androidstudio.com.gitceshi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
